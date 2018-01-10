12a04f05d86a228820f2c7c5de7f0b121d45134f
BritishPandaChick <sbartley1080@gmail.com>
2018-01-10 16:47:33 -0500

    Update code file

--- round-town.js
+++ round-town.js
@@ -1,1 +1,19 @@
-ÿþ
+
+function sumMultiplyAndRound(num1, num2, num3) {
+  var sum = Math.floor(num1 + num2);
+  var product = Math.round(sum * num3);
+
+  return Math.ceil(product);
+}
+
+
+
+
+
+
+
+
+//EXAMPLE TESTS
+
+console.log(sumMultiplyAndRound(2, 3.5, 6.2))
+//OUTPUT: 31
