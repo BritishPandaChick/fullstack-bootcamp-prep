78689f27e0df140c03d23282227f9af2feb0af58
BritishPandaChick <sbartley1080@gmail.com>
2018-01-15 16:00:07 -0500

    Add function runner

---
+++ function-runner.js
@@ -0,0 +1,24 @@
+/* Write Code Here */
+
+function functionRunner(func, arg) {
+  return func(arg);
+}
+
+
+
+
+
+
+
+
+
+
+
+//EXAMPLE TEST
+
+function squareNum(x) {
+  return x*x;
+}
+
+console.log(functionRunner(squareNum, 4));
+// OUTPUT: 16
