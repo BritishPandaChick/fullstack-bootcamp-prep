0df0c1b281ca7696696e6d8a13971fb36e54a43b
BritishPandaChick <sbartley1080@gmail.com>
2018-01-22 14:06:54 -0500

    Add my-push challenge

---
+++ my-push.js
@@ -0,0 +1,17 @@
+function myPush(arr, newElement){
+  arr[arr.length] = newElement;
+  return arr;
+}
+
+
+
+
+
+
+
+
+
+//EXAMPLE TESTS
+
+console.log(myPush([1, 2, 3], 4));
+//Output: [1, 2, 3, 4] 
