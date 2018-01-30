50dc61e4de02f57460f0143b43130610244d3397
BritishPandaChick <sbartley1080@gmail.com>
2018-01-30 15:44:17 -0500

    Add objectifying-arrays

--- objectifying-arrays.js
+++ objectifying-arrays.js
@@ -1,1 +1,62 @@
-ÿþ
+function objectifier(arr) {
+
+  var obj = {};
+
+  for (var i=0; i < arr.length; i++) {
+  	var propName = arr[i][0];
+    obj[propName] = {};
+    var values = arr[i][1];
+
+    for (var j=0; j < values.length; j++) {
+    	if (j % 2 === 0) {
+      	obj[propName][values[j]] = values[j+1];
+      }
+    }
+  }
+  return obj;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EXAMPLE TEST
+
+var data = [
+  [ 'animals',
+    [
+      'dogs', ['Corgi', 'Sheltie'],
+      'cats', ['Tabby','Black'],
+      'pigs', ['Teacup']
+    ]
+  ],
+  [ 'guitars',
+    [
+      'Fender', ['Telecaster', 'Stratocaster','Jazzmaster'],
+      'Gibson', ["Gibson Flying-V", 'Firebird', 'Les Paul']
+    ]
+  ]
+]
+
+console.log(objectifier(data));
+//OUTPUT:
+//
+// {
+// animals : {
+//     dogs : ['Corgi', 'Sheltie'],
+//     cats : ['Tabby','Black'],
+//     pigs : ['Teacup']
+// },
+// guitars : {
+//     Fender : ['Telecaster', 'Stratocaster','Jazzmaster'],
+//     Gibson : ["Gibson Flying-V", 'Firebird', 'Les Paul']
+// }
+// }
